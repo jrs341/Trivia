a5bc210e014fb5ffb18e73891991d33d06e44e32
Jason Sinn <jrs341@me.com>
2016-09-08 15:56:30 -0500

    added a constructor for the questions and answers not sure if its going to work

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+function questionAnswer(question, choices[], answer); {
+	this.question = question;
+	this.choices[] = choices[];
+	this.answer = answer;
+}
+
+var q1 = new questionAnswer('what is the inncorrect pets name', ['bear', 'sicily', 'gearmo', 'hoddy'], 'hoody');
+var q2 = new questionAnswer('which one is NaN', ['one', 'two', 'dog', 'three'], 'dog');

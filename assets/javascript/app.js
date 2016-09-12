aedad1c519510b6dcb6990e780e9a3bafe54d062
Jason Sinn <jrs341@me.com>
2016-09-12 05:26:36 -0500

    added a count down function need to move it out of question timer to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,7 +60,7 @@ var questionAnswer = [
 		correct: '3'
 	}
 ];
-// var click = true;
+var time = 5;
 
 var count = 0;
 
@@ -89,13 +89,11 @@ function firstQuestion() {
 	$('button').on('click', function() {
 		console.log($(this).text());
 		if ($(this).text() == questionAnswer[count].correct) {
-			console.log('correct');
 			correct++;
 			clearTimeout(questionTimeout);
 			firstQuestion();
 			
 		} else {
-			console.log('incorrect');
 			incorrect++;
 			clearTimeout(questionTimeout);
 			firstQuestion();	
@@ -112,14 +110,36 @@ function nextQuestion() {
 		// display results page here for a few moments
 		$('#question').empty();
 		$('#answer').empty();
-		$('#question').html('Correct Answers' + correct);
-		$('#answer').html('Incorrect Answers' + incorrect);
+		$('#question').html('Correct Answers: ' + correct);
+		$('#answers').html('Incorrect Answers: ' + incorrect);
 		count = 0;
+		correct = 0;
+		incorrect = 0;
 	}
+}
+
+function countDown() {
+	time--;
+	var converted = timeConverter(time);
+	$('#timer').html(converted);
+}
+
+function timeConverter(t){
+	var minutes = Math.floor(t/60);
+	var seconds = t - (minutes * 60);
+	 	if (seconds < 10){
+	    	seconds = "0" + seconds;
+	  	}
+	  	if (minutes === 0){
+	    	minutes = "00";
+	  } else if (minutes < 10){
+	   		 minutes = "0" + minutes;
+	  }
+	  	return minutes + ":" + seconds;
 }
 
 $(document).ready(function() {
 
-	// firstQuestion();
+	firstQuestion();
 
 });
